b5757a8590dda49dc0d4cd52fd81872f31f0684f
EricWang <wwssllabcd@gmail.com>
2016-12-29 00:34:42 +0800

    first init

---
+++ DesignPattern/DesignPatternDlg.h
@@ -0,0 +1,33 @@
+
+// DesignPatternDlg.h : 標頭檔
+//
+
+#pragma once
+
+
+// CDesignPatternDlg 對話方塊
+class CDesignPatternDlg : public CDialogEx
+{
+// 建構
+public:
+	CDesignPatternDlg(CWnd* pParent = NULL);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DESIGNPATTERN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
